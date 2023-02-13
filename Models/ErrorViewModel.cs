b6c3205abe7b63bb1392eda72ead00b70424d2d1
Dimitar Vasilev <mitko.v@mail.bg>
2023-02-13 12:12:14 +0200

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureLearnAppService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
